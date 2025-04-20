f55764e97023babe3ce06cb74d60b2210fc3a8be
Semyon Protas <semyon.protas@gmail.com>
2025-04-20 18:02:27 +0300

    nvf update

--- nvf_module.nix
+++ nvf_module.nix
@@ -1,6 +1,14 @@
 {pkgs, ...}: 
 {
   config.vim = {
-    theme.enable = true;
+    theme = {
+      enable = true;
+      name = "dracula";
+      style = "dark";
+    };
+
+    statusline.lualine.enable = true;
+    telescope.enable = true;
+    autocomplete.nvim-cmp.enable = true;
   };
 }
