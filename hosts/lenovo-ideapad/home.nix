3b9c08a65f1f6805ae6200615da996e306ee2b8d
Semyon Protas <semyon.protas@gmail.com>
2025-04-27 19:16:21 +0300

    Enable Yandex Music

--- hosts/lenovo-ideapad/home.nix
+++ hosts/lenovo-ideapad/home.nix
@@ -31,7 +31,10 @@
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
     # # fonts?
     # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
+    (yandex-music.override {
+      trayEnabled = true;
+      electronArguments = "";
+    })
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
     # # environment:
