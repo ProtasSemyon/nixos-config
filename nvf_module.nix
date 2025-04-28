7f5c733eda6c7b148e9a30ac6e5d381d7d681700
Semyon Protas <semyon.protas@gmail.com>
2025-04-28 10:58:47 +0300

    enable some languages support in nvf

--- nvf_module.nix
+++ nvf_module.nix
@@ -10,5 +10,14 @@
     statusline.lualine.enable = true;
     telescope.enable = true;
     autocomplete.nvim-cmp.enable = true;
+
+    languages = {
+      enableLSP = true;
+      enableTreesitter = true;
+
+      nix.enable = true;
+      ts.enable = true;
+      rust.enable = true;
+    };
   };
 }
